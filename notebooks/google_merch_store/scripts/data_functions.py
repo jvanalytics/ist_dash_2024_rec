a3aa8f3e2ec17f2af1c927f8954a781de6b4ad91
jvanalytics <joao.valente.work@gmail.com>
2024-10-11 16:55:48 +0100

    final commit mv ga4 merch

--- notebooks/google_merch_store/scripts/data_functions.py
+++ notebooks/google_merch_store/scripts/data_functions.py
@@ -396,5 +396,30 @@ def knn_study(
     return best_model, best_params
 
 
+from numpy import ndarray
+from pandas import DataFrame, read_csv
+from matplotlib.pyplot import savefig, show, figure
+from dslabs_functions import plot_multibar_chart, CLASS_EVAL_METRICS, run_NB, run_KNN
+
+
+def evaluate_approach(
+    train: DataFrame, test: DataFrame, target: str = "returning_user", metric: str = "accuracy"
+) -> dict[str, list]:
+    trnY = train.pop(target).values
+    trnX: ndarray = train.values
+    tstY = test.pop(target).values
+    tstX: ndarray = test.values
+    eval: dict[str, list] = {}
+
+    eval_NB: dict[str, float] = run_NB(trnX, trnY, tstX, tstY, metric=metric)
+    eval_KNN: dict[str, float] = run_KNN(trnX, trnY, tstX, tstY, metric=metric)
+    if eval_NB != {} and eval_KNN != {}:
+        for met in CLASS_EVAL_METRICS:
+            eval[met] = [eval_NB[met], eval_KNN[met]]
+    return eval
+
+
+
+
 print("data_functions lodaded")
 
