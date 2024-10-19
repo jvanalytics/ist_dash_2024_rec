1254c66e5ec1bd757652ca4baeeb9d7fc3a12e10
jvanalytics <93225097+jvanalytics@users.noreply.github.com>
2024-10-19 17:37:18 +0100

    updated full data prep script. adjusted target class to session number>2

--- notebooks/google_merch_store/00_ga4_merch_full_prep.py
+++ notebooks/google_merch_store/00_ga4_merch_full_prep.py
@@ -1,17 +1,30 @@
 import pandas as pd
 import numpy as np
-import scripts.dslabs_functions
-import scripts.config
+from datetime import datetime
 
-import scripts.data_functions
+import sys
+import os
 
-filepath=r'data/df_merch_2_encoded.csv'
+# Append the path of the scripts folder to Python path
+sys.path.append(os.path.join(os.path.dirname(__file__), 'scripts'))
+
+
+import dslabs_functions
+import config
+import data_functions
+
+
+
+# Append the path of the data folder to Python path
+filepath = os.path.join(os.path.dirname(__file__), 'data', 'df_merch_2_encoded.csv')
+# filepath=r'df_merch_2_encoded.csv'
+
 file_tag = "df_merch_full_prep"
 
 
 
-test_data=True
-# test_data=False
+# test_data=True
+test_data=False
 
 if test_data==True:
 
@@ -33,35 +46,60 @@ target='returning_user'
 # "When in the presence of temporal data, data partition shall use older data to train and newer to test, in order to not use future data to classify past data. In any other case, partition shall be random."
 data.sort_values(by='day_of_year', inplace=True)
 
+print(f"Encoded Data Load completed at: {datetime.now()}")
 
 
 # missing values
 
-data=apply_missing_values_frequent(data)
+data=data_functions.apply_missing_values_frequent(data)
 
 
 
 
 # outliers
 
-var='engagement_time_msec'
-summary5=data[[var]].describe(include="all")
+# no improvement
 
-data=drop_outliers(data, summary5, var)
+# var='engagement_time_msec'
+# summary5=data[[var]].describe(include="all")
+
+# data=data_functions.drop_outliers(data, summary5, var)
 
 
 # scaling
 
-data=apply_min_max_scaler(data,target)
-
+data=data_functions.apply_standard_scaler(data,target)
 
 
 # feature engineering
 
-
-
-# balancing
+data=data_functions.apply_remove_low_variance_variables(data,max_threshold=0.03, target=target, min_features_to_keep=10, exclude=['day_of_year'])
 
 
 
-print(data.info)
+print(f"Data Prep completed at: {datetime.now()}")
+print(data.head())
+
+# evaluation prep
+
+# Call the function to split the data
+y, X, labels, class_counts = data_functions.define_target_and_prepare_data(data, target=target)
+
+
+train, test = data_functions.split_data_save_csv(pd.DataFrame(X, columns=data.columns), y, data_columns=data.columns, target_column=target)
+
+
+# balancing training after splitting
+train=data_functions.apply_balanced_smote(train)
+
+
+# NB+KNN evaluate
+
+# Print current date and time
+print(f"Current time before final NB/KNN evaluation: {datetime.now()}")
+
+eval_metric='precision'
+eval_final: dict[str, list] = dslabs_functions.evaluate_approach(train, test, target='returning_user', metric=eval_metric)
+
+print(f'final evaluation: {eval_final}')
+print(f"Current time after final NB/KNN evaluation: {datetime.now()}")
